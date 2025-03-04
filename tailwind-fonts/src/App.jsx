9d8a60a02e04ad915cf3df117b03d1f723067935
John Serrano <andrey_201015@hotmail.com>
2025-03-04 14:22:27 -0500

    Add code tutorial tailwind fonts

---
+++ tailwind-fonts/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+// Supports weights 100-900
+import '@fontsource-variable/inter'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-6xl'>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        <p className='font-inter text-lg font-bold'>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+    </>
+  )
+}
+
+export default App
